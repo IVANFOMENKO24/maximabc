e62f3187633a9a2c6335b427bac0dfdbcb51ed1a
IVANFOMENKO24 <ivanfomenko24@users.noreply.github.com>
2026-08-04 02:39:12 +0300

    Add letters folder with 33 Russian letters videos, use relative path in config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 BOT_TOKEN = "8922425438:AAHXfPwXmCuV3N5WqbvR7UjUeWpOrKxgrf8"
 
-LETTERS_FOLDER = r"C:\Users\иванка\Desktop\макс\Ваня\видео\1\2\доп\туда\адаптивчик\апатив\буквыы"
-
-CACHE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "file_ids_cache.json")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+LETTERS_FOLDER = os.path.join(BASE_DIR, "letters")
+CACHE_FILE = os.path.join(BASE_DIR, "file_ids_cache.json")
 
 RUSSIAN_LETTERS = [
     "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й",
